39bff2fe01dc12a0e1b2d3720a54c19e3c4eb37e
Joao-Kleber-Linhalis <jklinhalis@gmail.com>
2024-01-30 17:24:30 -0300

    Inicio da tela de listar lugares

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:great_places_app/utils/app_routes.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meus Lugares"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM);
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
